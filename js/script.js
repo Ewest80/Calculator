998e94d48f2e725639eaa5a463f60bdaa97751dc
EWest80 <2019900+Ewest80@users.noreply.github.com>
2023-05-01 15:40:35 -0400

    Finalize styling, for now Add displaying numbers and clearing functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-let num1 = 0;
-let num2 = 0;
-let operator = '';
+const previousOutput = document.querySelector('.previous-output');
+const currentOutput = document.querySelector('.current-output');
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const allClearBtn = document.getElementById('allClearBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+const equalsBtn = document.getElementById('equalsBtn');
 
+let operand1 = '';
+let operand2 = '';
+let operator = null;
+
+// Math Functions
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -18,6 +27,14 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+function clear() {
+  operand1 = '';
+  operand2 = '';
+  operator = null;
+  previousOutput.textContent = '';
+  currentOutput.textContent = '';
+}
+
 function operate(num1, operator, num2) {
   switch (operator) {
     case '+':
@@ -30,3 +47,16 @@ function operate(num1, operator, num2) {
       return divide(num1, num2);
   }
 }
+
+function appendNumber(number) {
+  if (number === '.' && currentOutput.textContent.includes('.')) return;
+  currentOutput.textContent += number;
+}
+
+numberBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    appendNumber(button.textContent);
+  });
+});
+
+allClearBtn.addEventListener('click', clear);
